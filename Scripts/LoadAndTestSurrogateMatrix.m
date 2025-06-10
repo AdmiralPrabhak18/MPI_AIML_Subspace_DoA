18a58651c5ebf3a41786390584edb65a015c136a
PRABHAKARAN SUDHA <E21157254@adxuser.com>
2025-06-10 16:35:26 -0700

    Find peaks of MUSIC spectra

--- Scripts/LoadAndTestSurrogateMatrix.m
+++ Scripts/LoadAndTestSurrogateMatrix.m
@@ -24,34 +24,47 @@ az_angs = linspace(az_extent(1), az_extent(2), 500);
 A=linear_dir_vec(N, d, az_angs);
 
 %Calculate MUSIC Spectrum of R_norm
-SssDim = min(length(theta), N-1);
-[U,S,V]=svd(R_norm);
-Un=U(:,SssDim+1:end);
-
+SssDim = min(length(theta), N-1); %Dimension of Signal Subspace
+[U,S,V]=svd(R_norm);  % Eigen Decomposition
+Un=U(:,SssDim+1:end); % Form Noise Subspace
+Pmus = [];
 for ii=1:length(az_angs)
-    a=A(:,ii);
-    Pmus(ii)=1/(a'*Un*Un'*a);
+    a=A(:,ii);  %Select a steering vector
+    Pmus(ii)=10*log10(abs(1/(a'*Un*Un'*a)));  % Calculate MUSIC spectra
 end
+% Find Peaks in MUSIC Spectrum
+[vals_mus, locs_mus] = findpeaks(Pmus);
+peaks_mus = az_angs(locs_mus);
 
 %Calculate Spatial Spectrum of X_ml
+Pml = [];
 for ii=1:length(az_angs)
     a=A(:,ii);
-    Psurr(ii)=1/(a'*X_ml*a);
+    Pml(ii)=10*log10(abs(1/(a'*X_ml*a))); % Calculate spatial spectra of Xml
 end
+% Find peaks estimated from Xml
+[vals_ml, locs_ml] = findpeaks(Pml);
+peaks_ml = az_angs(locs_ml);
 
 % Plot both spectra
 figure()
 yyaxis left
+
 hold on
-plot(az_angs, 10*log10(abs(Pmus)))
+scatter(peaks_mus, vals_mus + 5, 'vb')
+plot(az_angs, Pmus, 'b')
 for i = 1:length(theta)
     xline(theta(i), 'k--')
 end
 ylabel('MUSIC Pseudospectum (dB)')
 yyaxis right
-plot(az_angs, 10*log10(abs(Psurr)))
+plot(az_angs, Pml, 'r')
+scatter(peaks_ml, vals_ml + 5, 'vr')
 xlabel('Azimuth (deg)')
 ylabel('Networks Spatial Spectrum (dB)')
 grid on;grid minor
+
+disp(['Peaks From MUSIC: ',num2str(peaks_mus)])
+disp(['Peaks From Neural Networks: ',num2str(peaks_ml)])
 
 
--- Scripts/MUSIC.m
+++ Scripts/MUSIC.m
@@ -17,7 +17,11 @@ R=x*x'/K;                             % Formation of Sample Covariance Matrix
 Vs=U(:,1:SssDim);      % Signal Subspace
 Vn=U(:,SssDim+1:end);  % Noise Subspace
 
+R_inv = inv(R + eye(N)*8);
+[~,S_inv, ~] = svd(R_inv);
+
 eigValSpec=10*log10(diag(S)); % Eigenvalue Spectrum
+eigValInvSpec=10*log10(diag(S_inv)); % Eigenvalue Spectrum
 
 fprintf(1,'Signal Subspace Implemented= %2.0f, Number of Sources = %2.0f, Number of Elements =%2.0f\n',SssDim,length(theta), N);
 fprintf(1,'Num.Samples = %2.0f, snr = %2.0f, %2.0f, %2.0f \n',K,SNR);
@@ -27,15 +31,22 @@ fprintf(1,'Eigenvalues (dB) = [ %5.2f, %5.2f, %5.2f, %5.2f, %5.2f, %5.2f, %5.2f,
 
 A=linear_dir_vec(N,d,az_angs); %Array Manifold of Steering Vectors
 I=eye(N);
+Pmus = [];
 for ii=1:length(az_angs)
     a=A(:,ii); % Assess spectrum at the angle value corresponding to this particular steering vector
-    Pmus(ii)=1/(a'*Vn*Vn'*a); % Calculate MUSIC Spectrum Using Noise Subspace
-    Pmus_signalSubSpace(ii)=1/(a'*(I-Vs*Vs')*a); % Calculate Spectrum Using Signal Subspace
+    Pmus(ii)=10*log10(abs(1/(a'*Vn*Vn'*a))); % Calculate MUSIC Spectrum Using Noise Subspace
+    Pmus_signalSubSpace(ii)=10*log10(abs(1/(a'*(I-Vs*Vs')*a))); % Calculate Spectrum Using Signal Subspace
 end
+% Find Peaks in MUSIC Spectrum
+[vals_mus, locs_mus] = findpeaks(Pmus);
+peaks_mus = az_angs(locs_mus);
+['Peaks From MUSIC: ',num2str(peaks_mus)]
 
 %Plot EigenValue Spectrum
 figure(1)
+hold on
 plot(eigValSpec, 'ko-','LineWidth',2)
+plot(eigValInvSpec, 'go-','LineWidth',2)
 title('Eigenvalue Spectrum')
 xlabel('Eigenvalues')
 ylabel('Magnitude (dB)')
@@ -47,7 +58,7 @@ ax.XTick = unique(round(ax.XTick));
 % Plot MUSIC Spectrum
 figure(2)
 hold on
-plot(az_angs,10*log10(Pmus),'b', az_angs,10*log10(Pmus_signalSubSpace),'r--','LineWidth',2);
+plot(az_angs,(Pmus),'b', az_angs,(Pmus_signalSubSpace),'r--','LineWidth',2);
 for i = 1:length(theta)
     xline(theta(i), '--')
 end
@@ -57,4 +68,6 @@ xlabel('Azimuth (deg)');
 ylabel('PseudoSpectrum (dB)');
 title('MUSIC Spectrum')
 legend('Nss V_{n}','Sss V_{s}', 'Truth DoAs')
+
+
 
